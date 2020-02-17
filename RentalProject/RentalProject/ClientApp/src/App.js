1200454dc66bd205dfe15d15a3f52029f1a7cbac
Owen <xiaofei.wang93@gmail.com>
2020-02-17 15:12:41 +1300

    added notfound component

--- RentalProject/RentalProject/ClientApp/src/App.js
+++ RentalProject/RentalProject/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import RentalList from './components/RentalList';
 import EditItem from './components/EditItem';
 import Login from './components/Login';
+import NotFound from './components/NotFound';
 
 import './custom.css'
 
@@ -18,6 +19,7 @@ export default class App extends Component {
         <Route path='/rental-list' component={RentalList} />
         <Route path='/edititem' component={EditItem} />
         <Route path='/login' component={Login} />
+        <Route path='/notfound' component={NotFound} />
       </Layout>
     );
   }
---
+++ RentalProject/RentalProject/ClientApp/src/components/NotFound.js
@@ -0,0 +1,11 @@
+﻿import styles from './NotFound.css';
+import React from 'react'
+
+export default function() {
+    return (
+        <div>
+            {/* <Exception type="404"  backText="Back to Home" /> */}
+            <h1>not found</h1>
+        </div>
+    );
+}
